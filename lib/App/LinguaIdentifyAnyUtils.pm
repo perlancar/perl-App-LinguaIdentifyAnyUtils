5681d757e1d116630a76a853e650f72e4530563f
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2019-07-10 22:40:30 +0700

    Release v0.001

---
+++ lib/App/LinguaIdentifyAnyUtils.pm
@@ -0,0 +1,25 @@
+package App::LinguaIdentifyAnyUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Command-line utilities related to Lingua::Identify::Any
+
+=for Pod::Coverage .+
+
+=head1 SYNOPSIS
+
+This distribution provides the following command-line utilities:
+
+#INSERT_EXECS_LIST
+
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+L<Lingua::Identify::Any>
+
+=cut
